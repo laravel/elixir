1bed4ed35ae06cf3eca2a2ac954f40d222ba9fd4
Taylor Otwell <taylorotwell@gmail.com>
2014-10-10 10:54:22 -0500

    Update Gulpfile.example.js

--- Gulpfile.example.js
+++ Gulpfile.example.js
@@ -9,10 +9,6 @@ var elixir = require('./vendor/laravel/elixir/Elixir');
  | Gulp tasks for your Laravel application. Elixir supports
  | several common CSS, JavaScript and even testing tools!
  |
- | In addition, Elixir will monitor your files for changes and
- | rebuild your scanned route and event configurations each
- | time you update your code. It is quick and convenient!
- |
  */
 
 elixir(function(mix) {
