a3f69a00f2a53ea133b26e31db2ef5fd02ac5a92
Jeffrey Way <jeffrey@jeffrey-way.com>
2014-10-14 00:21:44 -0400

    Cant auto-queue task

--- index.js
+++ index.js
@@ -7,8 +7,6 @@ Elixir.config = require('./Config');
 
 Elixir.extend = function(name, callback) {
 	Elixir.config[name] = callback;
-
-	Elixir.config.tasks.push(name);
 };
 
 module.exports = Elixir;
