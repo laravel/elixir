77464c615b0fd2640eedd2954cd9c8cb3dfc6f44
JeffreyWay <jeffrey@jeffrey-way.com>
2015-03-10 19:24:21 -0400

    When versioning files, copy over .map files as well.

--- ingredients/version.js
+++ ingredients/version.js
@@ -4,6 +4,8 @@ var rev = require('gulp-rev');
 var del = require('del');
 var utilities = require('./commands/Utilities');
 var vinylPaths = require('vinyl-paths');
+var fs = require('fs');
+var parsePath = require('parse-filepath');
 
 /*
  |----------------------------------------------------------------
@@ -17,13 +19,30 @@ var vinylPaths = require('vinyl-paths');
  */
 
 elixir.extend('version', function(src, buildDir) {
+    buildTask(src, buildDir);
 
+    this.registerWatcher('version', src);
+
+    return this.queueTask('version');
+});
+
+
+/**
+ * Build the "version" Gulp task.
+ *
+ * @param  {string} src
+ * @param  {string} buildDir
+ * @return {object}
+ */
+var buildTask = function(src, buildDir) {
     src = utilities.prefixDirToFiles('public', src);
-    buildDir = buildDir ? buildDir + '/build' : 'public/build';
+    buildDir = getBuildDir(buildDir);
 
     gulp.task('version', function() {
         var files = vinylPaths();
 
+        // To start, we'll clear out the build directory,
+        // so that we can start from scratch.
         del.sync(buildDir + '/*', { force: true });
 
         return gulp.src(src, { base: './public' })
@@ -38,11 +57,49 @@ elixir.extend('version', function(src, buildDir) {
                 // usually gets put in the "build" folder,
                 // alongside the suffixed version.
                 del(files.paths);
+
+                // We'll also copy over relevant sourcemap files.
+                copyMaps(src, buildDir);
             });
     });
+};
 
-    this.registerWatcher('version', src);
 
-    return this.queueTask('version');
+/**
+ * Prepare the path to the build directory.
+ *
+ * @param  {string} buildDir
+ * @return {string}
+ */
+var getBuildDir = function(buildDir) {
+    return buildDir ? buildDir + '/build' : 'public/build';
+};
 
-});
+
+/**
+ * Copy source maps to the build directory.
+ *
+ * @param  {string} src
+ * @param  {string} buildDir
+ * @return {object}
+ */
+var copyMaps = function(src, buildDir) {
+
+    // We'll first get any files from the src
+    // array that have companion .map files.
+    var mappings = src.map(function(file) {
+        var map = file + '.map';
+
+        if (fs.existsSync(map)) {
+            return map;
+        }
+    });
+
+    // And then we'll loop over this mapping array
+    // and copy each over to the build directory.
+    mappings.forEach(function(mapping) {
+        var map = mapping.replace('public', buildDir);
+
+        gulp.src(mapping).pipe(gulp.dest(parsePath(map).dirname));
+    });
+};
