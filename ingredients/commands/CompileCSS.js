bf76685adc2ac6a8c966006a4a1ac5d6074f21e5
JeffreyWay <jeffrey@jeffrey-way.com>
2015-04-04 10:44:33 -0400

    Restructure module a bit

--- ingredients/commands/CompileCSS.js
+++ ingredients/commands/CompileCSS.js
@@ -4,48 +4,68 @@ var config = require('laravel-elixir').config;
 var utilities = require('./Utilities');
 var Notification = require('./Notification');
 
-module.exports = function(options) {
+/**
+ * Display a compilation error notification.
+ *
+ * @param {object} e
+ */
+var onError = function(e) {
+    new Notification().error(e, options.compiler + ' Compilation Failed!');
 
-    var name = options.compiler.toLowerCase();
+    this.emit('end');
+};
 
-    var src = utilities.buildGulpSrc(
-        options.src, config.assetsDir + name, options.search
+
+/**
+ * Trigger Sass compilation.
+ *
+ * @param {mixed}  src
+ * @param {object} options
+ */
+var triggerSass = function(src, options) {
+    var toMaps = plugins.if(config.sourcemaps, plugins.sourcemaps.init());
+
+    if (options.plugin == 'gulp-ruby-sass') {
+        return require('gulp-ruby-sass')(src, options.pluginOptions).pipe(toMaps);
+    }
+
+    return gulp.src(src).pipe(toMaps).pipe(
+        plugins[options.plugin](options.pluginOptions)
     );
+};
 
-    var triggerSass = function(src) {
-        var toMaps = plugins.if(config.sourcemaps, plugins.sourcemaps.init());
 
-        if (options.plugin == 'gulp-ruby-sass') {
-            return require('gulp-ruby-sass')(src, options.pluginOptions).pipe(toMaps);
-        }
-
-        return gulp.src(src).pipe(toMaps).pipe(
-            plugins[options.plugin](options.pluginOptions)
-        );
-    };
-
-    var onError = function(e) {
-        new Notification().error(e, options.compiler + ' Compilation Failed!');
-
-        this.emit('end');
-    };
-
+/**
+ * Build the Gulp task.
+ *
+ * @param {string} name
+ * @param {mixed}  src
+ * @param {object} options
+ */
+var buildTask = function(name, src, options) {
     gulp.task(name, function() {
         utilities.logTask("Running " + options.compiler, src);
 
-        return triggerSass(src).on('error', onError)
+        return triggerSass(src, options).on('error', onError)
             .pipe(plugins.autoprefixer())
             .pipe(plugins.if(config.production, plugins.minifyCss()))
             .pipe(plugins.if(config.sourcemaps, plugins.sourcemaps.write('.')))
             .pipe(gulp.dest(options.output || config.cssOutput))
             .pipe(new Notification().message(options.compiler + ' Compiled!'));
     });
+}
 
-    config.registerWatcher(
-        name,
-        config.assetsDir + name + '/' + options.search
+
+module.exports = function(options) {
+    var name = options.compiler.toLowerCase();
+    var src = utilities.buildGulpSrc(
+        options.src, config.assetsDir + name, options.search
     );
+    var watchPath = config.assetsDir + name + '/' + options.search;
 
-    return config.queueTask(name);
+    buildTask(name, src, options);
 
+    return config
+        .registerWatcher(name, watchPath)
+        .queueTask(name);
 };
