7065207386a3bef91dea93e5d0bc9cf8c6274665
JeffreyWay <jeffrey@jeffrey-way.com>
2015-06-05 05:20:14 -0400

    Refactor copying of files

--- ingredients/commands/CopyFiles.js
+++ ingredients/commands/CopyFiles.js
@@ -1,58 +1,27 @@
-var gulp = require('gulp');
+var gulp   = require('gulp');
 var rename = require('gulp-rename');
 var gulpif = require('gulp-if');
 var config = require('laravel-elixir').config;
-var parsePath = require('parse-filepath');
-var _ = require('underscore');
-
-/**
- * Parse the Gulp source path.
- *
- * @param  {string} src
- * @return {object}
- */
-var parseSrc = function(src) {
-    var path = parsePath(src);
-    var isDir = (path.basename == path.name);
-
-    return _.extend(path, {
-        path: src + (isDir ? '/**/*' : ''),
-        isDir: isDir
-    });
-};
-
-
-/**
- * Parse the Gulp destination path.
- *
- * @param  {string} dest
- * @return {object}
- */
-var parseDest = function(dest) {
-    var path = parsePath(dest);
-    var isDir = (path.basename == path.name);
-
-    return _.extend(path, { path: isDir ? dest : path.dirname, isDir: isDir });
-};
 
 
 /**
  * Create the Gulp task.
- *
- * @return {void}
  */
 var buildTask = function() {
-    var stream;
-
     gulp.task('copy', function() {
-        config.duplicate.forEach(function(toCopy) {
-            stream = gulp
-                    .src(toCopy.src.path)
-                    .pipe(gulpif( ! toCopy.dest.isDir, rename(toCopy.dest.basename)))
-                    .pipe(gulp.dest(toCopy.dest.path));
-        });
+        return merge.apply(this, config.collections.copy.map(function(data) {
+            data.src.isDir && (data.src.path += '/**');
 
-        return stream;
+            utilities.logTask(
+                'Copying ' + data.src.path + ' to',
+                data.dest.path
+            );
+
+            return gulp
+                .src(data.src.path)
+                .pipe(gulpif( ! data.dest.isDir, rename(data.dest.name)))
+                .pipe(gulp.dest(data.dest.baseDir));
+        }));
     });
 };
 
@@ -61,9 +30,9 @@ module.exports = function(src, dest) {
     src = Array.isArray(src) ? src : [src];
 
     src.forEach(function(src) {
-        config.duplicate.push({
-            src: parseSrc(src),
-            dest: parseDest(dest)
+        config.saveTask('copy', {
+            src: utilities.parse(src),
+            dest: utilities.parse(dest)
         });
     });
 
