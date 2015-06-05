39f51cc0ff34cd23247b7b0231493f04f2659de7
JeffreyWay <jeffrey@jeffrey-way.com>
2015-06-05 05:55:03 -0400

    Refactor RunTests module

--- ingredients/commands/RunTests.js
+++ ingredients/commands/RunTests.js
@@ -1,7 +1,7 @@
-var config = require('laravel-elixir').config;
+var gulp         = require('gulp');
 var Notification = require('./Notification');
-var plugins = require('gulp-load-plugins')();
-var gulp = require('gulp');
+var plugins      = require('gulp-load-plugins')();
+var config       = require('laravel-elixir').config;
 
 
 /**
@@ -9,16 +9,16 @@ var gulp = require('gulp');
  *
  * @param {object} options
  */
-var buildTask = function(options) {
+module.exports = function(options) {
     var name = options.pluginName;
     var notify = new Notification();
 
     gulp.task(name, function() {
-        return gulp.src(options.src)
+        return gulp
+            .src(options.src)
             .pipe(plugins[name]('', options.pluginOptions))
             .on('error', function(e) {
                 notify.forFailedTests(e, options.framework);
-
                 this.emit('end');
             })
             .pipe(notify.forPassedTests(options.framework));
@@ -29,6 +29,3 @@ var buildTask = function(options) {
         .queueTask(name);
 };
 
-module.exports = function(options) {
-    return buildTask(options);
-};
