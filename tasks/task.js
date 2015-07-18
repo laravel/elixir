513f7a779d3f70c4b287f11364413d3a5430d140
Jeffrey Way <=>
2015-07-18 15:27:51 -0400

    Add 'task' task
    Return ability to do mix.task(‘custom-task’)

---
+++ tasks/task.js
@@ -0,0 +1,24 @@
+var gulp = require('gulp');
+var Elixir = require('laravel-elixir');
+
+
+/*
+ |----------------------------------------------------------------
+ | Mixing Custom Gulp Tasks
+ |----------------------------------------------------------------
+ |
+ | Sometimes, you'll want to hook your custom Gulp tasks into
+ | Elixir. Simple! Simply call Elixir's task() method, and
+ | provide the name of your task, and a regex to watch.
+ |
+ */
+
+Elixir.extend('task', function(name, watcher) {
+    var task = new Elixir.Task('task', function() {
+        return gulp.start(name);
+    });
+
+    if (watcher) {
+        task.watch(watcher);
+    }
+});
