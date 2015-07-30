6a6a68829e861f2320ec64f9840340a82b9278a5
Jeffrey Way <=>
2015-07-29 21:13:14 -0400

    Start adding tests

---
+++ elixir-test-app/test/sass.js
@@ -0,0 +1,68 @@
+var fs     = require('fs');
+var gulp   = require('gulp');
+var remove = require('rimraf');
+var should = require('chai').should();
+var Elixir = require('laravel-elixir');
+
+
+describe('Sass Task', function() {
+
+    it('compiles a single Sass file', done => {
+        Elixir(mix => mix.sass('app.scss'));
+
+        runGulp(() => {
+            shouldExist('./public/css/app.css');
+
+            done();
+        });
+    });
+
+
+    it('compiles to the source file name, if a single file is given', done => {
+        Elixir(mix => mix.sass('another.scss'));
+
+        runGulp(() => {
+            shouldExist('./public/css/another.css');
+
+            done();
+        });
+    });
+
+
+    it('compiles an array of Sass files', done => {
+        Elixir(mix => mix.sass(['app.scss', 'another.scss']));
+
+        runGulp(() => {
+            shouldExist('./public/css/app.css');
+
+            done();
+        });
+    });
+
+
+    it('saves to a custom directory and file name', done => {
+        Elixir(mix => mix.sass(['app.scss', 'another.scss'], './public/styles/done.css'));
+
+        runGulp(() => {
+            shouldExist('./public/styles/done.css');
+
+            done();
+        });
+    });
+
+});
+
+
+var shouldExist = (file) => {
+    return fs.existsSync(file).should.be.true;
+};
+
+
+var runGulp = assertions => {
+    gulp.start('default', () => {
+        assertions();
+
+        remove.sync('./public/css');
+        remove.sync('./public/styles');
+    });
+};
