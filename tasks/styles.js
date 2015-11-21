29731c9181a88a52d1f4fdb811750cc703046d4a
Jeffrey Way <jeffrey@jeffrey-way.com>
2015-11-21 10:31:57 -0500

    Finish removing concatCSS

--- tasks/styles.js
+++ tasks/styles.js
@@ -50,7 +50,7 @@ var gulpTask = function(paths) {
         gulp
         .src(paths.src.path)
         .pipe($.if(config.sourcemaps, $.sourcemaps.init()))
-        .pipe($.concatCss(paths.output.name, {rebaseUrls: false, inlineImports: false}))
+        .pipe($.concat(paths.output.name))
         .pipe($.if(config.production, $.minifyCss(config.css.minifyCss.pluginOptions)))
         .pipe($.if(config.sourcemaps, $.sourcemaps.write('.')))
         .pipe(gulp.dest(paths.output.baseDir))
