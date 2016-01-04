fe66b9ed356f4ef030716fbc4e99c6cd73d22cd4
Jeffrey Way <jeffrey@jeffrey-way.com>
2016-01-04 16:24:06 -0500

    Add logging + watching to mix.exec()

--- tasks/exec.js
+++ tasks/exec.js
@@ -3,14 +3,31 @@ var Elixir = require('laravel-elixir');
 
 /*
  |----------------------------------------------------------------
- | Execute
+ | Shell Commands
  |----------------------------------------------------------------
+ | 
+ | Need to execute a shell script, as part of your compile 
+ | process? No problem. Elixir can help with that. Just
+ | call `mix.exec('command')`, and, bam, you're set!
  |
  */
 
-Elixir.extend('exec', function(command) {
-    new Elixir.Task('exec', function() {
-        return gulp.src('').pipe(Elixir.Plugins.shell(command));
+Elixir.extend('exec', function(command, watcher) {
+    var task = new Elixir.Task('exec', function() {
+        Elixir.Log
+            .heading('Triggering Command...')
+            .message(command);
+
+        return (
+            gulp
+            .src('')
+            .pipe(Elixir.Plugins.shell(command));
+        );
     });
+
+    if (watcher) {
+        task.watch(watcher);
+    }
 });
+
 
