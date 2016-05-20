5641ec288a13fb1131dc05a167ee41667d0aeec6
Jeffrey Way <jeffrey@jeffrey-way.com>
2016-05-20 14:50:31 -0400

    Merge mix.combine() and mix.compress()
    Turns out they’re basically the same thing. So we can merge them into
    one main task.

--- src/tasks/combine.js
+++ src/tasks/combine.js
@@ -9,22 +9,36 @@
  |
  */
 
-Elixir.extend('combine', function(src, output, baseDir) {
+Elixir.extend('combine', task);
+Elixir.extend('compress', task);
+
+
+/**
+ * The main Elixir task that both  mix.combine()
+ * and mix.compress() defer to.
+ *
+ * @param  {string|array} src
+ * @param  {string|null}  output
+ * @param  {string|null}  baseDir
+ */
+function task(src, output, baseDir) {
     let paths = prepGulpPaths(src, baseDir, output);
 
-    new Elixir.Task('combine', function($) {
+    new Elixir.Task('combine', function($, config) {
         this.log(paths.src, paths.output);
 
         return (
             gulp
             .src(paths.src.path)
             .pipe($.concat(paths.output.name))
+            .pipe($.if(config.production, minify(paths.output)))
             .pipe(gulp.dest(paths.output.baseDir))
+            .pipe(new Elixir.Notification('Assets Combined!'))
         );
     })
     .watch(paths.src.path)
     .ignore(paths.output.path);
-});
+}
 
 
 /**
@@ -36,7 +50,9 @@ Elixir.extend('combine', function(src, output, baseDir) {
  * @return {GulpPaths}
  */
 function prepGulpPaths(src, baseDir, output) {
-    if ( ! output || ! parse(output).ext) {
+    // If you gave us an array of src files, but no
+    // explicit output name, we need more specifics.
+    if (Array.isArray(src) && (! output || ! parse(output).ext)) {
         Elixir.fail(
             "Please update your Gulpfile, and add a full output path as the " +
             "second argument to your mix.combine() call. " +
@@ -45,7 +61,42 @@ function prepGulpPaths(src, baseDir, output) {
         );
     }
 
+    // If the user is compressing a single file, and
+    // didn't provide an output path, we will just
+    // tack .min onto the file.
+    if (! Array.isArray(src) && ! output) {
+         let segments = parse(src);
+
+        output = segments.path.replace(
+            segments.ext, `.min${segments.ext}`
+        );
+    }
+
     return new Elixir.GulpPaths()
         .src(src, baseDir)
         .output(output);
 };
+
+
+/**
+ * Retrieve the proper minifier.
+ *
+ * @param  {object} output
+ * @return {stream}
+ */
+function minify(output) {
+    if (output.extension == '.css') {
+        return require('./shared/CssMinifier').default();
+    }
+
+    if (output.extension == '.js') {
+        return Elixir.Plugins.uglify(
+            Elixir.config.js.uglify.options
+        );
+    }
+
+    Elixir.fail(
+        'Hmm, not sure how to compress this type of file. ' +
+        'Stick with CSS or JavaScript files!'
+    );
+}
