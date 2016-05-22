84c824b49d58238f0dd13fa6f1bf4c9a631953dd
Jeffrey Way <jeffrey@jeffrey-way.com>
2016-05-22 00:36:16 -0400

    Check param name

--- src/bootstrap/SetHooks.js
+++ src/bootstrap/SetHooks.js
@@ -3,20 +3,20 @@
  *
  * @type {Object}
  */
-Elixir.hooks   = { before: [], watch: [] };
+Elixir.hooks = { before: [], watch: [] };
 
 
 /**
  * Register an event handler for 'gulp watch' is executed.
  *
- * @param {function} func
+ * @param {function} handler
  */
-Elixir.onWatch = func => Elixir.hooks.watch.push(func);
+Elixir.onWatch = handler => Elixir.hooks.watch.push(handler);
 
 
 /**
  * Register an event handler for before all Gulp tasks run.
  *
- * @param {function} func
+ * @param {function} handler
  */
-Elixir.before  = func => Elixir.hooks.before.push(func);
+Elixir.before = handler => Elixir.hooks.before.push(handler);
