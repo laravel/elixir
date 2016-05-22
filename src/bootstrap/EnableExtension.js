84bcc79ab322be63459a492f10c0c2f93a25a869
Jeffrey Way <jeffrey@jeffrey-way.com>
2016-05-22 00:52:00 -0400

    Revert back to regular function

--- src/bootstrap/EnableExtension.js
+++ src/bootstrap/EnableExtension.js
@@ -4,7 +4,7 @@
  * @param {string}   name
  * @param {Function} callback
  */
-Elixir.extend = (name, callback) => {
+Elixir.extend = function (name, callback) {
     Elixir.mixins[name] = function() {
         callback.apply(this, arguments);
 
