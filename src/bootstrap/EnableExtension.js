3b5c62a05a54d8dae0dfb62b408100a17ad49895
Jeffrey Way <jeffrey@jeffrey-way.com>
2016-05-31 13:12:46 -0400

    Don't override existing extensions

--- src/bootstrap/EnableExtension.js
+++ src/bootstrap/EnableExtension.js
@@ -5,6 +5,11 @@
  * @param {Function} callback
  */
 Elixir.extend = function (name, callback) {
+    // If we've already registered a mixin of this name
+    // we'll return early. This way, extensions can
+    // override default behavior.
+    if (Elixir.mixins[name]) return;
+
     Elixir.mixins[name] = function() {
         callback.apply(this, arguments);
 
