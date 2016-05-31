c3e3baf75c55de06ff462a0b5d4c7c46baa795da
Jeffrey Way <jeffrey@jeffrey-way.com>
2016-05-31 13:12:51 -0400

    Update Browserify deprecated message

--- src/tasks/deprecated.js
+++ src/tasks/deprecated.js
@@ -22,8 +22,9 @@ Elixir.extend('browserify', function() {
             .heading('Compatibility Alert!')
             .heading(
                 `'mix.browserify()' was extracted to its own extension in
-                Laravel Elixir 6.0. You'll want to instead call 'mix.scripts().'
-                Alternatively, install 'laravel-elixir-browserify'.`.replace(/\s{2,}/g, "\n")
+                Laravel Elixir 6.0. Instead, either opt for 'mix.scripts()' -
+                which leverages Webpack - or install 'laravel-elixir-browserify-official'
+                like this: http://bit.ly/1UrRC87`.replace(/\s{2,}/g, "\n")
             );
     })
 });
