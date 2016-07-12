8badfcb104e2145f51d443418b342a19fb6cad3a
Jeffrey Way <jeffrey@jeffrey-way.com>
2016-07-12 15:24:02 -0400

    Add Stylus to install instructions

--- src/tasks/recipes/external-recipes.js
+++ src/tasks/recipes/external-recipes.js
@@ -1,5 +1,6 @@
 Elixir.registerInstallInstruction('rollup', 'npm install laravel-elixir-rollup-official --save-dev');
 Elixir.registerInstallInstruction('coffee', 'npm install laravel-elixir-coffeescript --save-dev');
+Elixir.registerInstallInstruction('stylus', 'npm install --save-dev laravel-elixir-stylus');
 Elixir.registerInstallInstruction('browserify', 'npm install laravel-elixir-browserify-official --save-dev');
 Elixir.registerInstallInstruction('webpack', 'npm install laravel-elixir-webpack-official --save-dev');
 Elixir.registerInstallInstruction('browserSync', 'npm install laravel-elixir-browsersync-official --save-dev');
